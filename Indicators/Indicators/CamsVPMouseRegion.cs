eb5e9e8d21729c0089ab2ccaa3b9d24fee4ef8cb
Paulo Hawk <paulloos2016@gmail.com>
2026-01-18 18:52:43 -0400

    Add files via upload

---
+++ Indicators/Indicators/CamsVPMouseRegion.cs
@@ -0,0 +1,41 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+
+#endregion
+
+//This namespace holds Drawing tools in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.DrawingTools
+{
+	[CLSCompliant(false)]
+	[EditorBrowsable(EditorBrowsableState.Always)]
+	public class CamsVpRegion : RegionHighlightBase
+	{
+		protected override void OnStateChange()
+		{
+			base.OnStateChange();
+			if (State == State.SetDefaults)
+			{
+				Name	= "CamsVp Region";
+				Mode	= RegionHighlightMode.Time;
+			}
+		}
+	}	
+}
